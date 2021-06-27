8fd782a9676ac8ad3924e38905082d39a87aa017
innjie <innjie@naver.com>
2021-06-27 18:24:23 +0900

    [add] DTO files

---
+++ src/main/java/com/example/blog/model/Board.java
@@ -0,0 +1,38 @@
+package com.example.blog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob //대용량 데이터
+    private String content;
+
+    @ColumnDefault("0")
+    private int count;
+
+    @ManyToOne
+    @JoinColumn(name="userId")
+    private User user;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+}
